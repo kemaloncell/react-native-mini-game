8ab217e5e8781a42f8f778a66eeb892b22d413b4
Kemal Öncel <kemaloncel3535@outlook.com>
2025-01-22 15:03:21 +0300

    create resuable title component and use SafeAreaView component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,31 @@
-import { Text } from 'react-native'
-
+import { Text, StyleSheet } from 'react-native'
+import Title from '../components/Title';
 function GameScreen() {
-    return <Text>Game Screen!!</Text>
+    return <View style={styles.screen}>
+        <Title>Opponent's Guess</Title>
+         GUESS
+         <View>
+            <Text>Higher or lower?</Text>
+            + -
+         </View>
+         <View>LOG ROUNDS</View>
+    </View>
 }
 
 export default GameScreen
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: '#ddb52f',
+        padding: 12,
+    }
+});
