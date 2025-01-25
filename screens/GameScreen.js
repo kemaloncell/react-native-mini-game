4608795e4ad46af3ccd2e4ed0333c9f7fec44119
Kemal Öncel <kemaloncel3535@outlook.com>
2025-01-25 15:58:50 +0300

    create, use, display random numbers

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,30 @@
 import { Text, StyleSheet, View } from 'react-native'
-import Title from '../components/Title';
-function GameScreen() {
+import Title from '../components//ui/Title';
+import { useState } from 'react';
+import NumberContainer from '../components/game/NumberContainer';
+
+function GameScreen({ userNumber }) {
+    const initalNumber = generateRandomNumber(1, 100, userNumber)
+    const [currentGuess, setCurrentGuess] = useState(initalNumber)
+
+    function generateRandomNumber(min, max, exclude) {
+        // Math.random() may produce  0 so I added min if it is become 0
+        const rndNum = Math.floor(Math.random() * (max - min)) + min
+
+        if(rndNum === exclude) {
+            return generateRandomNumber(min, max, exclude)
+        } else {
+            return rndNum
+        }
+    }
+
+
     return <View style={styles.screen}>
         <Title>Opponent's Guess</Title>
-        {/*GUESS*/}
+        <NumberContainer>{currentGuess}</NumberContainer>
          <View>
             <Text>Higher or lower?</Text>
-            + -
+           {/* + - */}
          </View>
          <View>{/*LOG ROUNDS*/}</View>
     </View>
